99497ec626bbeea5f1b562e335a20eab91a17132
zlatkoiliev <83271119+zlatkoiliev@users.noreply.github.com>
2022-01-25 15:35:00 +0200

    remove packages

--- src/old-components/GoogleMap/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'load-google-maps-api' {
-  export interface GoogleMapsLoaderOptions {
-    key: string;
-  }
-
-  declare function loadGoogleMapsApi(options: GoogleMapsLoaderOptions): Promise<void>;
-
-  export default loadGoogleMapsApi;
-}
